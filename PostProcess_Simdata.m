268c55cb1254ef7aa9387bb73bd56bcbc2206085
Devon Cox <u20458208@tuks.co.za>
2025-07-19 12:28:45 +0200

    Made main.m script a function

--- PostProcess_Simdata.m
+++ PostProcess_Simdata.m
@@ -1,6 +1,16 @@
 clear; close all; clc
 
-run('main.m')
+numBrushes  = 20;
+isRolling   = true;
+fs_sim      = 1e4;
+fs_save     = 1e3;
+t_initial   = 0;
+t_final     = 120;
+
+sim_solution = main(numBrushes, isRolling, fs_sim, fs_save, t_initial, t_final);
+
+
+t_save = single( linspace(t_initial, t_final, t_final * fs_save + 1) );
 v0 = model_input.v0;
 omega = model_input.omega;
 dt_sim = model_input.dt_sim;
--- main.m
+++ main.m
@@ -1,32 +1,23 @@
-clear; close all; clc;
-set(0, 'DefaultFigureWindowStyle', 'docked')
-%%
-
-clearvars; clc;
-close all;
-
-total_time = tic;
-pressure_start = tic; 
-fprintf("Starting simulation. \n")
-fs_save = 1e2; % Hz
-fs_sim = 1e3; % Hz
-numBrushes = 20;
-t_initial = 0;
-t_final = 120;
-isRolling = false;
-t_save = single( linspace(t_initial, t_final, t_final * fs_save + 1) );
-
-model_input = brush_init(numBrushes, isRolling, fs_sim, fs_save, t_initial, t_final);
-
-fprintf("Initialised brush model simulation in %.2fs! \n", toc(pressure_start))
-
-K = min(size(model_input.omega), [], 'all');
-sim_solution = cell(1, K);
-
-%%%%%%%% Simulate brush model %%%%%%%%%%%%%%%%%%%%%
-for i = 1:K
-    sim_solution{i} = simulateBrushModel_V2(model_input);
+function [model_input, sim_solution] = main(numBrushes, isRolling, fs_sim, fs_save, t_initial, t_final)
+    clearvars; clc;
+    close all;
+    
+    total_time = tic; 
+    fprintf("Starting simulation. \n")
+    
+    model_input = brush_init(numBrushes, isRolling, fs_sim, fs_save, t_initial, t_final);
+    
+    fprintf("Initialised brush model simulation in %.2fs! \n", toc(pressure_start))
+    
+    K = min(size(model_input.omega), [], 'all');
+    sim_solution = cell(1, K);
+    
+    %%%%%%%% Simulate brush model %%%%%%%%%%%%%%%%%%%%%
+    for i = 1:K
+        sim_solution{i} = simulateBrushModel_V2(model_input);
+    end
+    toc(total_time)
+    load gong.mat
+    sound(y, Fs)
 end
 
-load gong.mat
-sound(y, Fs)
