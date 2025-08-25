483af7e60fa4c093cff84caff740194a79ffc667
Devon Cox <u20458208@tuks.co.za>
2025-08-25 07:38:58 +0200

    Started implementing EKF to test viability for stiff system integration

---
+++ Utility/extendedKalmanFilter.m
@@ -0,0 +1,14 @@
+function [X_next, KG, PC] = extendedKalmanFilter(t, X, Y)
+%EXTENDEDKALMANFILTER uses the EKF formulation to step a non-linear
+%function forward by 1 time step
+%   t: current time
+%   X: Current state vector
+%   Y: Current Measurment vector
+%
+%   X_next: Predicted state vector
+%   KG: Kalman gain matrix
+%   PC: Prediction Covariance matrix
+
+
+end
+
