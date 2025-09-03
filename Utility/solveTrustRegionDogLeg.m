962b194eb59d5a0bebfb776661b24b841d0ff065
Devon Cox <u20458208@tuks.co.za>
2025-09-03 15:49:00 +0200

    Removed pre-conditioning step in dogleg method

--- Utility/solveTrustRegionDogLeg.m
+++ Utility/solveTrustRegionDogLeg.m
@@ -78,22 +78,22 @@ function [x_next, Fty_next, exitFlag, output] = solveTrustRegionDogLeg(func, t, 
         % Check conditioning of Hessian
         Hk_cond = rcond(Hk);
         
-        if Hk_cond >= 1e-3
+        if Hk_cond >= 1e-6
             % Compute unconstrained minimiser step
             p_gn = -1 * decomposition(Hk) \ g;
         
-        elseif Hk_cond < 1e-6 % Apply a pre-conditioner
-            % Get diagonal. This will serve as the pre-conditioner
-            D = diag(Hk);
-            D = D + 1e-3;
-            
-            % Create pre-conditioned Hessian
-            Hk_prec = Hk ./ D;
-
-            % Solve pre-conditioned system
-            g_prec = g ./ D;
-            p_gn_prec = -decomposition(Hk_prec) \ g_prec;
-            p_gn = p_gn_prec;
+        % % elseif Hk_cond < 1e-6 % Apply a pre-conditioner
+        % %     % Get diagonal. This will serve as the pre-conditioner
+        % %     D = diag(Hk);
+        % %     D = D + 1e-3;
+        % % 
+        % %     % Create pre-conditioned Hessian
+        % %     Hk_prec = Hk ./ D;
+        % % 
+        % %     % Solve pre-conditioned system
+        % %     g_prec = g ./ D;
+        % %     p_gn_prec = -decomposition(Hk_prec) \ g_prec;
+        % %     p_gn = p_gn_prec;
         else
             p_gn = -1 * pinv(Hk) * g;
         end
